fdbeb5b20e894ed6950cc04954ec5fafdbc3c96e
Aldimhr <dirikuin16@gmail.com>
2022-09-23 21:21:05 +0700

    feat: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./components/**/*.{js,jsx}', './pages/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
